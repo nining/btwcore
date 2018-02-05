8326903a99c15e631b5a5dd361728ef0f2c4fcaf
nining <nining@gmail.com>
2018-02-05 04:10:45 +0000

    fix more multiple changes

--- src/utilmoneystr.cpp
+++ src/utilmoneystr.cpp
@@ -14,9 +14,9 @@ std::string FormatMoney(const CAmount& n)
     // Note: not using straight sprintf here because we do NOT want
     // localized number formatting.
     int64_t n_abs = (n > 0 ? n : -n);
-    int64_t quotient = n_abs/COIN;
-    int64_t remainder = n_abs%COIN;
-    std::string str = strprintf("%d.%08d", quotient, remainder);
+    int64_t quotient = n_abs/(COIN / MULTIPLE);
+    int64_t remainder = n_abs%(COIN / MULTIPLE);
+    std::string str = strprintf("%d.%04d", quotient, remainder);
 
     // Right-trim excess zeros before the decimal point:
     int nTrim = 0;
@@ -48,7 +48,7 @@ bool ParseMoney(const char* pszIn, CAmount& nRet)
         if (*p == '.')
         {
             p++;
-            int64_t nMult = CENT*10;
+            int64_t nMult = (CENT / MULTIPLE)*10;
             while (isdigit(*p) && (nMult > 0))
             {
                 nUnits += nMult * (*p++ - '0');
@@ -67,10 +67,10 @@ bool ParseMoney(const char* pszIn, CAmount& nRet)
             return false;
     if (strWhole.size() > 10) // guard against 63 bit overflow
         return false;
-    if (nUnits < 0 || nUnits > COIN)
+    if (nUnits < 0 || nUnits > (COIN / MULTIPLE))
         return false;
     int64_t nWhole = atoi64(strWhole);
-    CAmount nValue = nWhole*COIN + nUnits;
+    CAmount nValue = nWhole*(COIN / MULTIPLE) + nUnits;
 
     nRet = nValue;
     return true;
